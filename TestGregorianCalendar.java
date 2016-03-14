e3a0db66029ac7f806850a6685cab839e777f840
LiaoKunZhi <a0956823675@gmail.com>
2016-03-14 21:01:49 +0800

    Create TestGregorianCalendar.java-1
    First,make a main method to start the work,and create two variables to let user save the choose.

---
+++ TestGregorianCalendar.java
@@ -0,0 +1,16 @@
+//U10416043   廖崑智
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class TestGregorianCalendar {
+
+	//Main method
+	public static void main(String[] args) {
+		Calendar calendar = new GregorianCalendar();
+
+		int year;
+		int month;
+		}
+	}
+}
