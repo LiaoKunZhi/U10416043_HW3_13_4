537a5f2d1b339fc2abb4f4d18c491316564c104b
LiaoKunZhi <a0956823675@gmail.com>
2016-03-14 21:08:53 +0800

    Create TestGregorianCalendar.java-2
    Make the title and the body of the calendar,and display it to user.

--- TestGregorianCalendar.java
+++ TestGregorianCalendar.java
@@ -13,4 +13,21 @@ public class TestGregorianCalendar {
 		int month;
 		}
 	}
+		//Print the calendar for a month in a year
+	public static void printMonth(int year, int month) {
+
+		//Print the headings of the calendar
+		printMonthTitle(year, month);
+
+		//Print the body of the calendar
+		printMonthBody(year, month);
+	}
+
+	//Print the month title, e.g., March 2012
+	public static void printMonthTitle(int year, int month) {
+
+		System.out.println("         " + getMonthName(month) + " " + year);
+		System.out.println("----------------------------");
+		System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
+	}
 }
